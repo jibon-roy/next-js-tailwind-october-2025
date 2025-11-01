5a069baf9f908082de7b768bbaee59eeb2c8f1eb
Jibon Krishna Roy <mailme.jibon@gmail.com>
2025-11-01 08:56:53 +0600

    problem solved and update code

--- src/lib/helpers/envConfig.ts
+++
@@ -1,5 +0,0 @@
-export const envConfig = {
-  baseApi: "https://jsonplaceholder.typicode.com",
-  api_key: process.env.NEXT_PUBLIC_api_key,
-  google_api_key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-};
